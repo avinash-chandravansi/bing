c9f1f8e3d65d8bed8d77d037afc43a537dcc22bb
avinash-chandravansi <avinash.chandravansi@outlook.com>
2023-04-11 02:35:52 +0530

    new project 8

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 3000;
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
   });
+
+  app.get('/add', function(req, res) {
+    console.log(req.query);
+    res.send(req.query);
+  });
   
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
