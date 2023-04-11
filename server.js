ab48127aace38e3eb704d5787b7f687788084903
avinash-chandravansi <avinash.chandravansi@outlook.com>
2023-04-11 14:48:03 +0530

    new projectghfddehsd

--- server.js
+++ server.js
@@ -2,15 +2,43 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
-
+const fs = require('fs');
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
   });
 
+  app.get('/fetch', function(req, res) {
+
+
+    fs.readFile('./data.json', 'utf8', (err, data) => {
+      if (!err) {
+         res.JSON(data)
+         
+      }
+      else
+      {
+        res.send("oops!! something crashed")
+      }
+  });
+  });
 
   app.get('/add', function(req, res) {
-    console.log(req.query);
-    res.send(req.query);
+    fs.readFile('./data.json', 'utf8', (err, data) => {
+      if (!err) {
+          let jsonArray = JSON.parse(data);
+          jsonArray.push(req.query);
+          let json = JSON.stringify(jsonArray);
+          fs.writeFile('./data.json', json, (err) => {
+              if (!err) {
+                  console.log('done');
+                  res.send("Done");
+              }
+          });
+      }
+  });
+
+   
+    
   });
   
 app.listen(PORT, () => {
